80841727dbd990ea24b459e577648084345c6b5c
stevenromero98 <stevenromero98@users.noreply.github.com>
2018-11-25 05:19:19 +0800

    First Commit

---
+++ app/src/main/java/com/example/nutz/romero_finals_labact2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nutz.romero_finals_labact2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
